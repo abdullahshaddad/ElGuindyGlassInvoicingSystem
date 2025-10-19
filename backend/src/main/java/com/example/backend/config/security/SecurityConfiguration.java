74067ccd7277514bfba9e450152fc29279616103
abdullahshaddad <147054749+abdullahshaddad@users.noreply.github.com>
2025-10-19 16:52:44 +0300

    corsconfig vercel

--- backend/src/main/java/com/example/backend/config/security/SecurityConfiguration.java
+++ backend/src/main/java/com/example/backend/config/security/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration {
                             "http://localhost:5173",
                             "http://localhost:5174",
                             "http://localhost:3000",
-                            "https://elguindyglassinvoicingsystem.onrender.com"
+                            "https://el-guindy.vercel.app"
                     ));
                     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
