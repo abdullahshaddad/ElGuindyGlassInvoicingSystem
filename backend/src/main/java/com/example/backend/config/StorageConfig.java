ac4189dbfa1a3f2d01dd18c0604749fc26779a9a
MarwanMohamed01 <110474519+MarwanMohamed01@users.noreply.github.com>
2025-10-19 03:34:57 +0300

    add: disable minio service

---
+++ backend/src/main/java/com/example/backend/config/StorageConfig.java
@@ -0,0 +1,33 @@
+package com.example.backend.config;
+
+import com.example.backend.services.DisabledStorageService;
+import com.example.backend.services.StorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StorageConfig {
+    @Bean
+    @ConditionalOnProperty(
+            name = "minio.enabled",
+            havingValue = "false",
+            matchIfMissing = true
+    )
+    public DisabledStorageService disabledStorageService() {
+        return new DisabledStorageService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "minio.enabled", havingValue = "true")
+    public StorageService minioStorageService(
+            @Value("${minio.endpoint}") String endpoint,
+            @Value("${minio.accessKey}") String accessKey,
+            @Value("${minio.secretKey}") String secretKey,
+            @Value("${minio.bucketName}") String bucketName,
+            @Value("${minio.publicUrl}") String publicUrl
+    ) {
+        return new StorageService(endpoint, accessKey, secretKey, bucketName, publicUrl);
+    }
+}
